73b1d309e2a0504f4c75d2a5438550ccac243490
steedude <85280987+steedude@users.noreply.github.com>
2024-06-21 16:24:34 +0800

    itty-router test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { AutoRouter } from 'itty-router'; // ~1kB
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+const router = AutoRouter();
+
+router
+	.get('/', () => `Hello, world!`)
+	.get('/hello/:name', ({ name }) => `Hello, ${name}!`)
+	.get('/json', () => [1, 2, 3])
+	.get('/promises', () => Promise.resolve('foo'));
+
+export default router;
